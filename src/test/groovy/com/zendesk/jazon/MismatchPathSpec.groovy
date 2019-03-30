9fd0b09bf17054f452909f80640a2f74658ecc84
Pawel Mikolajczyk <pmikolajczyk@zendesk.com>
2019-03-30 19:53:00 +0100

    Cleanup MismatchPathSpec extracting large Maps into constants

--- src/test/groovy/com/zendesk/jazon/MismatchPathSpec.groovy
+++ src/test/groovy/com/zendesk/jazon/MismatchPathSpec.groovy
@@ -46,182 +46,8 @@ class MismatchPathSpec extends Specification {
 
     def "complex case 2"() {
         given:
-        def expected = [
-                data: [
-                        [
-                                key   : 111,
-                                values: [
-                                        a       : 1,
-                                        b       : "one",
-                                        elements: [
-                                                [
-                                                        name: "color",
-                                                        value: "blue"
-                                                ],
-                                                [
-                                                        name: "width",
-                                                        value: 5
-                                                ],
-                                                [
-                                                        name: "height",
-                                                        value: 10
-                                                ],
-                                        ]
-                                ]
-                        ],
-                        [
-                                key   : 111,
-                                values: [
-                                        a: 2,
-                                        b: "two",
-                                        elements: [
-                                                [
-                                                        name: "color",
-                                                        value: "green"
-                                                ],
-                                                [
-                                                        name: "width",
-                                                        value: 12
-                                                ],
-                                                [
-                                                        name: "height",
-                                                        value: 24
-                                                ],
-                                        ]
-                                ]
-                        ],
-                        [
-                                key   : 111,
-                                values: [
-                                        a: 3,
-                                        b: "three",
-                                        elements: [
-                                                [
-                                                        name: "color",
-                                                        value: "red"
-                                                ],
-                                                [
-                                                        name: "width",
-                                                        value: 33
-                                                ],
-                                                [
-                                                        name: "height",
-                                                        value: 66
-                                                ],
-                                        ]
-                                ]
-                        ],
-                        [
-                                key   : 111,
-                                values: [
-                                        a: 4,
-                                        b: "four",
-                                        elements: [
-                                                [
-                                                        name: "color",
-                                                        value: "orange"
-                                                ],
-                                                [
-                                                        name: "width",
-                                                        value: 25
-                                                ],
-                                                [
-                                                        name: "height",
-                                                        value: 50
-                                                ],
-                                        ]
-                                ]
-                        ],
-                ]
-        ]
-        def actual = [
-                data: [
-                        [
-                                key   : 111,
-                                values: [
-                                        a       : 1,
-                                        b       : "one",
-                                        elements: [
-                                                [
-                                                        name: "color",
-                                                        value: "blue"
-                                                ],
-                                                [
-                                                        name: "width",
-                                                        value: 5
-                                                ],
-                                                [
-                                                        name: "height",
-                                                        value: 10
-                                                ],
-                                        ]
-                                ]
-                        ],
-                        [
-                                key   : 111,
-                                values: [
-                                        a: 2,
-                                        b: "two",
-                                        elements: [
-                                                [
-                                                        name: "color",
-                                                        value: "black"
-                                                ],
-                                                [
-                                                        name: "width",
-                                                        value: 12
-                                                ],
-                                                [
-                                                        name: "height",
-                                                        value: 24
-                                                ],
-                                        ]
-                                ]
-                        ],
-                        [
-                                key   : 111,
-                                values: [
-                                        a: 3,
-                                        b: "three",
-                                        elements: [
-                                                [
-                                                        name: "color",
-                                                        value: "red"
-                                                ],
-                                                [
-                                                        name: "width",
-                                                        value: 33
-                                                ],
-                                                [
-                                                        name: "height",
-                                                        value: 66
-                                                ],
-                                        ]
-                                ]
-                        ],
-                        [
-                                key   : 111,
-                                values: [
-                                        a: 4,
-                                        b: "four",
-                                        elements: [
-                                                [
-                                                        name: "color",
-                                                        value: "orange"
-                                                ],
-                                                [
-                                                        name: "width",
-                                                        value: 25
-                                                ],
-                                                [
-                                                        name: "height",
-                                                        value: 50
-                                                ],
-                                        ]
-                                ]
-                        ],
-                ]
-        ]
+        def expected = VERY_COMPLEX_OBJECT
+        def actual = VERY_COMPLEX_OBJECT_WITH_ONE_FIELD_DIFFERENT
 
         when:
         def result = match(expected, actual)
@@ -238,4 +64,182 @@ class MismatchPathSpec extends Specification {
                 .actual(actual)
                 .match()
     }
+
+    private static final VERY_COMPLEX_OBJECT = [
+            data: [
+                    [
+                            key   : 111,
+                            values: [
+                                    a       : 1,
+                                    b       : "one",
+                                    elements: [
+                                            [
+                                                    name: "color",
+                                                    value: "blue"
+                                            ],
+                                            [
+                                                    name: "width",
+                                                    value: 5
+                                            ],
+                                            [
+                                                    name: "height",
+                                                    value: 10
+                                            ],
+                                    ]
+                            ]
+                    ],
+                    [
+                            key   : 111,
+                            values: [
+                                    a: 2,
+                                    b: "two",
+                                    elements: [
+                                            [
+                                                    name: "color",
+                                                    value: "green"
+                                            ],
+                                            [
+                                                    name: "width",
+                                                    value: 12
+                                            ],
+                                            [
+                                                    name: "height",
+                                                    value: 24
+                                            ],
+                                    ]
+                            ]
+                    ],
+                    [
+                            key   : 111,
+                            values: [
+                                    a: 3,
+                                    b: "three",
+                                    elements: [
+                                            [
+                                                    name: "color",
+                                                    value: "red"
+                                            ],
+                                            [
+                                                    name: "width",
+                                                    value: 33
+                                            ],
+                                            [
+                                                    name: "height",
+                                                    value: 66
+                                            ],
+                                    ]
+                            ]
+                    ],
+                    [
+                            key   : 111,
+                            values: [
+                                    a: 4,
+                                    b: "four",
+                                    elements: [
+                                            [
+                                                    name: "color",
+                                                    value: "orange"
+                                            ],
+                                            [
+                                                    name: "width",
+                                                    value: 25
+                                            ],
+                                            [
+                                                    name: "height",
+                                                    value: 50
+                                            ],
+                                    ]
+                            ]
+                    ],
+            ]
+    ]
+
+    private static final VERY_COMPLEX_OBJECT_WITH_ONE_FIELD_DIFFERENT = [
+            data: [
+                    [
+                            key   : 111,
+                            values: [
+                                    a       : 1,
+                                    b       : "one",
+                                    elements: [
+                                            [
+                                                    name: "color",
+                                                    value: "blue"
+                                            ],
+                                            [
+                                                    name: "width",
+                                                    value: 5
+                                            ],
+                                            [
+                                                    name: "height",
+                                                    value: 10
+                                            ],
+                                    ]
+                            ]
+                    ],
+                    [
+                            key   : 111,
+                            values: [
+                                    a: 2,
+                                    b: "two",
+                                    elements: [
+                                            [
+                                                    name: "color",
+                                                    value: "black"
+                                            ],
+                                            [
+                                                    name: "width",
+                                                    value: 12
+                                            ],
+                                            [
+                                                    name: "height",
+                                                    value: 24
+                                            ],
+                                    ]
+                            ]
+                    ],
+                    [
+                            key   : 111,
+                            values: [
+                                    a: 3,
+                                    b: "three",
+                                    elements: [
+                                            [
+                                                    name: "color",
+                                                    value: "red"
+                                            ],
+                                            [
+                                                    name: "width",
+                                                    value: 33
+                                            ],
+                                            [
+                                                    name: "height",
+                                                    value: 66
+                                            ],
+                                    ]
+                            ]
+                    ],
+                    [
+                            key   : 111,
+                            values: [
+                                    a: 4,
+                                    b: "four",
+                                    elements: [
+                                            [
+                                                    name: "color",
+                                                    value: "orange"
+                                            ],
+                                            [
+                                                    name: "width",
+                                                    value: 25
+                                            ],
+                                            [
+                                                    name: "height",
+                                                    value: 50
+                                            ],
+                                    ]
+                            ]
+                    ],
+            ]
+    ]
 }
