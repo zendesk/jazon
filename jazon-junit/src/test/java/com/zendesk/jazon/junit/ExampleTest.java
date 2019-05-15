5dbfe1fc5b9faf2ce234816360b1d0fed7cae490
Pawel Mikolajczyk <pmikolajczyk@zendesk.com>
2019-05-15 08:24:52 +0200

    Create adapter for JUnit

---
+++ jazon-junit/src/test/java/com/zendesk/jazon/junit/ExampleTest.java
@@ -0,0 +1,56 @@
+package com.zendesk.jazon.junit;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.zendesk.jazon.junit.JazonJunitAdapter.assertThat;
+import static java.util.Arrays.asList;
+
+
+public class ExampleTest {
+
+    @Test(expected = AssertionError.class)
+    public void simpleTest() {
+        // given
+        String actualJson = "{\"value\": 123}";
+        Map<String, Object> expectedJsonAsMap = ImmutableMap.<String, Object>builder()
+                .put("value", 50)
+                .build();
+
+        // then
+        assertThat(actualJson).matches(expectedJsonAsMap);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testWithNestedArray() {
+        // given
+        String actualJson = "{" +
+                "\"value\": 50," +
+                "\"tags\": [\"blue\", \"black\", \"red\"]" +
+                "}";
+
+        // then
+        assertThat(actualJson).matches(
+                deal(50, asList("blue", "pink", "red"))
+        );
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testWithRootArray() {
+        // given
+        String actualJson = "[\"blue\", \"black\", \"red\"]";
+
+        // then
+        assertThat(actualJson).matches(asList("blue", "pink", "red"));
+    }
+
+    private static Map<String, Object> deal(int value, List<String> tags) {
+        return ImmutableMap.<String, Object>builder()
+                .put("value", value)
+                .put("tags", tags)
+                .build();
+    }
+}
