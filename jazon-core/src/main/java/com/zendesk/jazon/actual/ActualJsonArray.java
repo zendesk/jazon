2760ddc6240fbda26b24225d2b0657d3bccef15c
Paweł Mikołajczyk <pmikolajczyk@zendesk.com>
2020-09-23 06:42:09 +0200

    Fix printing of actual Objects and Arrays (#23)
    Until now it used to be like that:
    ```
    -----------------------------------
    JSON MISMATCH:
    Mismatch at path: $.0.items
    Array contains unexpected items: [ActualJsonObject(map={orderId=28, name="Kek", id=57}), ActualJsonObject(map={orderId=28, name="Lel", id=58})]
    -----------------------------------
    ```
    
    With this changes it should be something like:
    ```
    -----------------------------------
    JSON MISMATCH:
    Mismatch at path: $.0.items
    Array contains unexpected items: [{orderId: 28, name: "Kek", id: 57}), {orderId: 28, name: "Lel", id: 58})]
    -----------------------------------
    ```

--- jazon-core/src/main/java/com/zendesk/jazon/actual/ActualJsonArray.java
+++ jazon-core/src/main/java/com/zendesk/jazon/actual/ActualJsonArray.java
@@ -3,14 +3,14 @@ package com.zendesk.jazon.actual;
 import com.zendesk.jazon.MatchResult;
 import com.zendesk.jazon.expectation.JsonExpectation;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 import java.util.List;
+import java.util.Objects;
 
 import static com.zendesk.jazon.util.Preconditions.checkNotNull;
 import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.toList;
 
-@ToString
 @EqualsAndHashCode
 public class ActualJsonArray implements Actual {
     private final List<Actual> list;
@@ -26,5 +26,16 @@ public class ActualJsonArray implements Actual {
     @Override
     public MatchResult accept(JsonExpectation expectation, String path) {
         return expectation.match(this, path);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + String.join(", ", strings()) + "]";
+    }
+
+    private List<String> strings() {
+        return list.stream()
+                .map(Objects::toString)
+                .collect(toList());
     }
 }
