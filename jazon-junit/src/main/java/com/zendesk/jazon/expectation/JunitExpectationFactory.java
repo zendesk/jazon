9d16b86239fadabe60f4faaccc1eaca92f2f82c3
Pawel Mikolajczyk <pmikolajczyk@zendesk.com>
2020-09-26 07:07:25 +0200

    Fix compilation errors

--- jazon-junit/src/main/java/com/zendesk/jazon/expectation/JunitExpectationFactory.java
+++ jazon-junit/src/main/java/com/zendesk/jazon/expectation/JunitExpectationFactory.java
@@ -24,7 +24,7 @@ public class JunitExpectationFactory implements ExpectationFactory {
     @Override
     public JsonExpectation expectation(Object object) {
         if (object instanceof Map) {
-            return objectExpectation((Map<String, Object>) object, this);
+            return objectExpectation((Map<CharSequence, Object>) object, this);
         } else if (object instanceof Number) {
             return new PrimitiveValueExpectation<>(new ActualJsonNumber((Number) object));
         } else if (object instanceof String) {
