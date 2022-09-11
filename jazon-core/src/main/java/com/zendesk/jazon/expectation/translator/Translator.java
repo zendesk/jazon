59de93eb0b582d28f365d23579716ac015cdae1a
Pawel Mikolajczyk <pmikolajczyk@zendesk.com>
2022-09-11 08:54:40 +0200

    Make `Translator` interface public
    # Why?
    Because enforcing all third-party interface implementations being inside `com.jazon.expectation.translator` package is considered controversial.
    
    I have never seen such pattern anywhere. Although it seems sensible by supporting information-hiding pattern, I favour making it similar to all other things that people seen in Java.

--- jazon-core/src/main/java/com/zendesk/jazon/expectation/translator/Translator.java
+++ jazon-core/src/main/java/com/zendesk/jazon/expectation/translator/Translator.java
@@ -2,6 +2,6 @@ package com.zendesk.jazon.expectation.translator;
 
 import com.zendesk.jazon.expectation.JsonExpectation;
 
-interface Translator<T> {
+public interface Translator<T> {
     JsonExpectation jsonExpectation(T object, TranslatorFacade translator);
 }
